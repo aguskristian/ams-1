bb0690c370554bc55139357097890ec98841ffb0
Ngurah Mantara <ngurahmantara@gmail.com>
2013-06-09 17:25:56 +0800

    local adjustment

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/gapura/ams/';
+$config['base_url']	= 'http://localhost/gapura/fids/';
 
 /*
 |--------------------------------------------------------------------------
