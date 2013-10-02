d4bf439ca04929f9c4231d1543a550ad62f765e6
ngurahmantara <ngurahmantara@gmail.com>
2013-10-02 14:36:36 +0800

    dashboard_v

---
+++ application/views/attendance/dashboard.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+            <div class="row-fluid">
+
+<div class="row-fluid">
+
+</div>
+
+<div>
+click here for <?php echo anchor('attendance/in','in'); ?>
+<br>
+click here for <?php echo anchor('attendance/out','out'); ?>
+</div>
